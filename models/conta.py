40d7a84fda6c9658400dfc7be8731d37fb88e068
Luiz Eduardo Lima Balbino <luizeduardo071205@gmail.com>
2026-02-07 15:23:13 -0300

    remove conflitos de merge e formatação no arquivo conta.py

--- models/conta.py
+++ models/conta.py
@@ -9,13 +9,8 @@ class Conta:
     nome: str
     tipo: str # -> (Ativo, Passivo, PL, Receita, Despesa)
     saldo: float = 0.0
-<<<<<<< HEAD
     descricao : Optional[str] = None
     ativa: bool
-=======
-    descricao: Optional[str] = None
-    ativa: bool 
->>>>>>> 73f728e2ad4ff55feee9c545c4a5d9f52ec66389
     
     def debitar(self, valor: float):
         self.saldo += valor
